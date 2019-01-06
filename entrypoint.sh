fecbfcefa898b4331e1143d1fc2832b5fee7e6b9
Denis Baryshev <dennybaa@gmail.com>
2019-01-06 13:56:06 +0300

    use custom hostname if given

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,29 @@ set -e
 SPARK_CONF_DIR="${SPARK_CONF_DIR:-"${SPARK_HOME}/conf"}"
 [ ! -f ${SPARK_CONF_DIR}/spark-env.sh ] || . ${SPARK_CONF_DIR}/spark-env.sh
 
+## Use the given domain name or hostname for naming a Spark node
+#
+if [ -n "${SPARK_DOMAIN}" ]; then
+  host_opts="-h $(hostname -s).${SPARK_DOMAIN}"
+elif [ -n "${SPARK_HOSTNAME}" ]; then
+  host_opts="-h ${SPARK_HOSTNAME}"
+fi
+
 ## Invocation shortcut commands
 #
 cmd=$1
 case $cmd in
 master|worker)
     shift
-    set -- ${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.${cmd}.${cmd^}" "$@"
-    exec /sbin/tini -- "$@" -i 0.0.0.0
+    set -- ${SPARK_HOME}/bin/spark-class "org.apache.spark.deploy.${cmd}.${cmd^}" $@
+    exec /sbin/tini -- $@ $host_opts
   ;;
 spark-shell|shell)
     shift
-    exec /sbin/tini -- ${SPARK_HOME}/bin/spark-shell "$@"
+    exec /sbin/tini -- ${SPARK_HOME}/bin/spark-shell $@
   ;;
 *)
     # Run an arbitary command
-    [ -n "$*" ] && exec "$@" || exec /bin/bash
+    [ -n "$*" ] && exec $@ || exec /bin/bash
   ;;
 esac
